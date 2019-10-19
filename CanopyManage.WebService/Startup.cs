1ed514b8065aa67b48049183bf514e8e0bef221c
Khuyen Tran <khuyen.tran@logixtek.com>
2019-10-19 12:51:42 +0700

    Add Swagger

--- CanopyManage.WebService/Startup.cs
+++ CanopyManage.WebService/Startup.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CanopyManage.Common.EventBus.Abstractions;
+﻿using CanopyManage.Common.Logger;
+using CanopyManage.WebService.Compositions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using CanopyManage.Common.Logger;
 
 namespace CanopyManage.WebService
 {
@@ -27,14 +21,31 @@ namespace CanopyManage.WebService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                 .AddControllersAsServices();
             services.AddApiVersioning();
-            services.RegisterLogger(Configuration["Logging:InstrumentationKey"]);
+            services.AddVersionedApiExplorer(options => options.GroupNameFormat = "'v'VVV");
+
+            services.RegisterLogger(Configuration["Logging:InstrumentationKey"])
+                    .AddSwagger();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApiVersionDescriptionProvider provider)
         {
+            // Enable middle-ware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                options =>
+                {
+                    foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        options.SwaggerEndpoint(
+                            $"/swagger/{description.GroupName}/swagger.json",
+                            description.GroupName.ToUpperInvariant());
+                    }
+                });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
