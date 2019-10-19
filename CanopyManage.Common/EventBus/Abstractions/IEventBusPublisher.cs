2b781861a6341e38af0d256b299831d545101e3c
Khuyen Tran <khuyen.tran@logixtek.com>
2019-10-19 09:45:43 +0700

    remove unused code

--- CanopyManage.Common/EventBus/Abstractions/IEventBusPublisher.cs
+++ CanopyManage.Common/EventBus/Abstractions/IEventBusPublisher.cs
@@ -6,10 +6,6 @@ namespace CanopyManage.Common.EventBus.Abstractions
 {
     public interface IEventBusPublisher
     {
-        Task PublishAsync(IntegrationEvent @event, IDictionary<string, string> userDictionary = null);
-
-        Task PublishAsync(IEnumerable<IntegrationEvent> eventList, IDictionary<string, string> userDictionary = null);
-
         Task PublishAsync(Event @event, string partitionKey, IDictionary<string, string> userDictionary = null);
 
         Task PublishAsync(IEnumerable<Event> eventList, string partitionKey, IDictionary<string, string> userDictionary = null);
