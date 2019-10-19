bce1fef7cfbbcf729e27e1056ea45fad24ff2515
Khuyen Tran <khuyen.tran@logixtek.com>
2019-10-19 15:32:09 +0700

    Add repository composition

---
+++ CanopyManage.Infrastructure/Compositions/Repository.cs
@@ -0,0 +1,22 @@
+﻿using CanopyManage.Domain.Aggregates;
+using CanopyManage.Domain.SeedWork;
+using CanopyManage.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CanopyManage.Infrastructure.Compositions
+{
+    public static class RepositoryComposition
+    {
+        public static IServiceCollection AddRepository(this IServiceCollection services)
+        {
+            //inject client
+
+            services.AddScoped<IRepository<AzureResource<ServiceNowServiceAccount>, string>, ServiceAccountKeyVaultRepository>();
+
+            return services;
+        }
+    }
+}
