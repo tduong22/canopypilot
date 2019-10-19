b842749a9f75d78084ea53ec15b1626228bdc2f0
Khuyen Tran <khuyen.tran@logixtek.com>
2019-10-19 11:04:45 +0700

    Add common logger

---
+++ CanopyManage.Common/Logger/LoggerExtensions.cs
@@ -0,0 +1,80 @@
+﻿using Ardalis.GuardClauses;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace CanopyManage.Common.Logger
+{
+    public static class LoggerExtensions
+    {
+        private const string CorrelationKey = "CorrelationId";
+        private const string PayloadKey = "Payload";
+
+        public static void CustomLogDebug(this ILogger logger, string message, Exception exception = null, string correlationId = "", object payload = null)
+        {
+            Guard.Against.Null(logger, nameof(logger));
+            logger.CustomLog(LogLevel.Debug, message, exception, correlationId, payload);
+        }
+
+        public static void CustomLogTrace(this ILogger logger, string message, Exception exception = null, string correlationId = "", object payload = null)
+        {
+            Guard.Against.Null(logger, nameof(logger));
+            logger.CustomLog(LogLevel.Trace, message, exception, correlationId, payload);
+        }
+
+        public static void CustomLogInformation(this ILogger logger, string message, Exception exception = null, string correlationId = "", object payload = null)
+        {
+            Guard.Against.Null(logger, nameof(logger));
+            logger.CustomLog(LogLevel.Information, message, exception, correlationId, payload);
+        }
+
+        public static void CustomLogWarning(this ILogger logger, string message, Exception exception = null, string correlationId = "", object payload = null)
+        {
+            Guard.Against.Null(logger, nameof(logger));
+            logger.CustomLog(LogLevel.Warning, message, exception, correlationId, payload);
+        }
+
+        public static void CustomLogError(this ILogger logger, string message, Exception exception = null, string correlationId = "", object payload = null)
+        {
+            Guard.Against.Null(logger, nameof(logger));
+            logger.CustomLog(LogLevel.Error, message, exception, correlationId, payload);
+        }
+
+        public static void CustomLogCritical(this ILogger logger, string message, Exception exception = null, string correlationId = "", object payload = null)
+        {
+            Guard.Against.Null(logger, nameof(logger));
+            logger.CustomLog(LogLevel.Critical, message, exception, correlationId, payload);
+        }
+
+        private static void CustomLog(this ILogger logger, LogLevel logLevel, string message, Exception exception = null, string correlationId = "", object payload = null)
+        {
+            Guard.Against.Null(logger, nameof(logger));
+            var customProperties = new Dictionary<string, object>();
+            if (!string.IsNullOrEmpty(correlationId))
+            {
+                customProperties.Add(CorrelationKey, correlationId);
+            }
+
+            if (payload != null)
+            {
+                string payloadJson;
+                try
+                {
+                    payloadJson = JsonConvert.SerializeObject(payload);
+                }
+                catch
+                {
+                    payloadJson = "Exception occurred while deserialize object to Json.";
+                }
+                customProperties.Add(PayloadKey, payloadJson);
+            }
+
+            logger.Log(logLevel, new EventId(9999),
+               customProperties
+               , exception,
+               (s, ex) =>
+                   message);
+        }
+    }
+}
---
+++ CanopyManage.Common/Logger/LoggerInjector.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.ApplicationInsights;
+using Microsoft.Extensions.Options;
+
+namespace CanopyManage.Common.Logger
+{
+    public static class LoggerInjector
+    {
+        public static IServiceCollection RegisterLogger(this IServiceCollection services, string instrumentationKey, LogLevel logLevel = LogLevel.Information)
+        {
+            services.AddLogging();
+
+            var filterOptions = new LoggerFilterOptions();
+            filterOptions.AddFilter(string.Empty, logLevel);
+            var config = TelemetryConfiguration.CreateDefault();
+            config.InstrumentationKey = instrumentationKey;
+
+            IOptions<TelemetryConfiguration> telemeryOptions = Options.Create(config);
+            IOptions<ApplicationInsightsLoggerOptions> configureApplicationInsightsLoggerOptions = Options.Create(
+                new ApplicationInsightsLoggerOptions());
+
+            ILoggerFactory loggerFactory =
+               new LoggerFactory(
+                   new[]
+                   {
+                        new ApplicationInsightsLoggerProvider(telemeryOptions,
+                            configureApplicationInsightsLoggerOptions)
+                   }, filterOptions);
+
+            services.AddSingleton<ILoggerFactory>(loggerFactory);
+
+            return services;
+        }
+    }
+}
