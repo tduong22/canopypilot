69093c544d3e5b3dec7f0bc4d9c1c25756e0aba9
Khuyen Tran <khuyen.tran@logixtek.com>
2019-10-19 11:05:07 +0700

    Register logger and add docker file

--- CanopyManage.WebService/Startup.cs
+++ CanopyManage.WebService/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using CanopyManage.Common.Logger;
 
 namespace CanopyManage.WebService
 {
@@ -27,6 +28,8 @@ namespace CanopyManage.WebService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.RegisterLogger(Configuration["Logging:InstrumentationKey"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
